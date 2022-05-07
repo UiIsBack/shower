4010206d2fdc9834f441d76989ebf90eeed2425e
UiIsBack <100056544+UiIsBack@users.noreply.github.com>
2022-05-07 01:15:08 +0100

    Delete shower.py

--- shower.py
+++
@@ -1,2 +0,0 @@
-def gooutside():
-    print("hello, it has come to my attention you have spent to much time being a skid and coding in copy paste, please refrain from these actions and take a shower then go outside <3")
