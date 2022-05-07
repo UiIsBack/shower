1d38b6bd3afd656563d71434c92c614145171b6c
UiIsBack <100056544+UiIsBack@users.noreply.github.com>
2022-05-07 01:14:53 +0100

    Create __init__.py

---
+++ shower/__init__.py
@@ -0,0 +1,2 @@
+def gooutside():
+    print("hello, it has come to my attention you have spent to much time being a skid and coding in copy paste, please refrain from these actions and take a shower then go outside <3")
